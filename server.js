1d082d2d8e3b4e7a39aa0a64950248712e88fcc5
Ismail Kildis <ismail.kildis@algomedi.com>
2022-06-20 08:56:12 +0300

    removed nodemodules

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
-  Book = require('./api/models/book'), 
-  bodyParser = require('body-parser');
-  
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Book = require('./api/models/book'),
+    bodyParser = require('body-parser');
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/LibraryDB'); 
+//mongoose.connect('mongodb://localhost:49156/LibraryDB'); 
+
+mongoose.connect('mongodb://mongodb:27017/LibraryDB');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,83 +22,5 @@ app.listen(port);
 
 console.log('Library API server started on: ' + port);
 
-book.insertMany([
-    {
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-},
-{
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-},
-{
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-},
-{
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-},
-{
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-},
-{
-    title: "Suç ve Ceza",
-    author: {
-        name : "Dostoyevski",
-        country : "Rusya",
-        birtdate : "1821-10-11"
-    },
-    price : "100",
-    isbn: "afwfwfw-111www",
-    language : "Turkce",
-    numberOfPages: 700
-}
-]).then(function(){
-    console.log("Data inserted")  // Success
-}).catch(function(error){
-    console.log(error)      // Failure
-});
 
 
